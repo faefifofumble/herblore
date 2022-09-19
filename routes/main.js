4fae3f03a3144a8dc340aafe49a65197e7fef9be
Faerlyn Hunter <faerlynathunter@gmail.com>
2022-09-18 21:10:03 -0500

    added nav on profile

--- routes/main.js
+++ routes/main.js
@@ -8,12 +8,12 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/settings", ensureAuth, postsController.getSettings);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
-router.get("/settings", authController.getSettings);
 
 module.exports = router;
